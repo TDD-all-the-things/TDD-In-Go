aefc38bbe3b28c9bd5fdc56172806615bda94ee1
longyue0521 <longyueli0521@gmail.com>
2022-03-17 15:47:06 +0800

    绿灯：不惜犯下罪恶快速使编译通过，此处可以理解为绿灯一闪而过 红灯：测试正常运行但断言失败

---
+++ stringcalculator/string_calculator.go
@@ -0,0 +1,12 @@
+package stringcalculator
+
+type StringCalculator struct {
+}
+
+func NewStringCalculator() *StringCalculator {
+	return nil
+}
+
+func (s *StringCalculator) Add(numbers string) int {
+	return -1
+}
