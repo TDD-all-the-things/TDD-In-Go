4349eb0eb3b2ced2cada45ca8b6a392fbeac5a97
longyue0521 <longyueli0521@gmail.com>
2022-03-17 16:25:32 +0800

    重构：测试代码也有"坏味道"，用表格驱动法编写Test_StringCalculator测试，重构测试代码并删除"重复"测试Test_Add_EmptyString_ReturnsZero以提高测试代码的可维护性

--- stringcalculator/string_calculator_test.go
+++ stringcalculator/string_calculator_test.go
@@ -6,14 +6,31 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_Add_EmptyString_ReturnsZero(t *testing.T) {
-	sc := NewStringCalculator()
-	assert.NotNil(t, sc)
-	assert.Equal(t, 0, sc.Add(""))
-}
-
 func Test_Add_SingleNumber_ReturnsThatNumbers(t *testing.T) {
 	sc := NewStringCalculator()
 	assert.NotNil(t, sc)
 	assert.Equal(t, 1, sc.Add("1"))
+}
+
+func Test_StringCalculator(t *testing.T) {
+
+	testcases := map[string]struct {
+		numbers  string
+		expected int
+	}{
+		"Add_EmptyString_ReturnsZero": {
+			numbers:  "",
+			expected: 0,
+		},
+	}
+
+	for name, tt := range testcases {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			sc := NewStringCalculator()
+			assert.NotNil(t, sc)
+			assert.Equal(t, tt.expected, sc.Add(tt.numbers))
+		})
+	}
+
 }
