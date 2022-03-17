f305a8931eeb7479463599833a163fe8b741b337
longyue0521 <longyueli0521@gmail.com>
2022-03-17 21:02:30 +0800

    重构：将Add方法形参由numbers重命名为template，Extract Method抽取parseTemplate方法

--- stringcalculator/string_calculator.go
+++ stringcalculator/string_calculator.go
@@ -14,13 +14,18 @@ func NewStringCalculator() *StringCalculator {
 
 func (s *StringCalculator) Add(template string) int {
 	sum := 0
-	delimiter, numbers := ",", template
-	if strings.HasPrefix(template, `//`) {
-		delimiter, numbers = ";", template[5:]
-	}
+	delimiter, numbers := s.parseTemplate(template)
 	for _, number := range strings.Split(strings.ReplaceAll(numbers, `\n`, delimiter), delimiter) {
 		num, _ := strconv.Atoi(number)
 		sum += num
 	}
 	return sum
+}
+
+func (s *StringCalculator) parseTemplate(template string) (delimiter string, numbers string) {
+	delimiter, numbers = ",", template
+	if strings.HasPrefix(template, `//`) {
+		delimiter, numbers = ";", template[5:]
+	}
+	return
 }
