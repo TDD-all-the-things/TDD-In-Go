7c248714d8b1fb9c90f7eb134aad3ecd3eb2eb4f
longyue0521 <longyueli0521@gmail.com>
2022-03-18 23:29:59 +0800

    绿灯：为Option结构体引入logging字段，在Parse方法中以遍历flags切片的方式将Option的logging字段设置为true

--- args/args.go
+++ args/args.go
@@ -1,19 +1,24 @@
 package args
 
 func Parse(option *Option, flags ...string) {
+	for _, flag := range flags {
+		if flag == "-l" {
+			option.logging = true
+		}
+	}
 }
 
 type Option struct {
+	logging bool
 }
 
 func (o *Option) Logging() bool {
-	return false
+	return o.logging
 }
 
 func (o *Option) Port() int {
 	return 0
 }
-
 func (o *Option) Directory() string {
 	return ""
 }
