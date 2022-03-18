cc172a9fbde90aa7c7530c01cf6feea01fc56506
longyue0521 <longyueli0521@gmail.com>
2022-03-18 23:32:56 +0800

    重构：利用Go表格驱动+Subtest特性，消除测试代码中的"重复"坏味道

--- args/args_test.go
+++ args/args_test.go
@@ -7,18 +7,38 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestParseOption_NoFlagsPassed_GetsDefaultValues(t *testing.T) {
-	var option args.Option
-	args.Parse(&option)
-	assert.Equal(t, false, option.Logging())
-	assert.Equal(t, 0, option.Port())
-	assert.Equal(t, "", option.Directory())
-}
-
 func TestParseOption_OnlyLoggingFlagPassed_GetsTrueForLoggingAndDefaultValuesForOthers(t *testing.T) {
 	var option args.Option
 	args.Parse(&option, "-l")
 	assert.Equal(t, true, option.Logging())
 	assert.Equal(t, 0, option.Port())
 	assert.Equal(t, "", option.Directory())
+}
+
+func TestParseOption(t *testing.T) {
+
+	testcases := map[string]struct {
+		flags    []string
+		expected args.Option
+	}{
+		"no flags": {
+			flags:    []string{},
+			expected: args.Option{},
+		},
+	}
+
+	for name, tt := range testcases {
+		t.Run(name, func(t *testing.T) {
+			// 注意:并发问题
+			tt := tt
+			// 利用多核,并行运行
+			t.Parallel()
+			
+			var actual args.Option
+			args.Parse(&actual, tt.flags...)
+			assert.Equal(t, tt.expected.Logging(), actual.Logging())
+			assert.Equal(t, tt.expected.Port(), actual.Port())
+			assert.Equal(t, tt.expected.Directory(), actual.Directory())
+		})
+	}
 }
