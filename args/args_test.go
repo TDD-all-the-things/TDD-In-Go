43f143cdecbe21681ec54764b48ddfe0c4d1cd80
longyue0521 <longyueli0521@gmail.com>
2022-03-25 12:23:37 +0800

    重构：将TestParseAnotherOption中的测试用例全部移入TestParseOption中（需要重命名），将TestParseOption重命名为TestParse，消除测试代码中的"重复"坏味道

--- args/args_test.go
+++ args/args_test.go
@@ -14,7 +14,7 @@ type Option struct {
 	Directory string
 }
 
-func TestParseOption(t *testing.T) {
+func TestParse(t *testing.T) {
 
 	testcases := map[string]struct {
 		flags    []string
@@ -24,21 +24,41 @@ func TestParseOption(t *testing.T) {
 			flags:    []string{},
 			expected: &Option{},
 		},
+		"no flags another": {
+			flags:    []string{},
+			expected: &AnotherOption{},
+		},
 		"-l only": {
 			flags:    []string{"-l"},
 			expected: &Option{true, 0, ""},
+		},
+		"-l only another": {
+			flags:    []string{"-l"},
+			expected: &AnotherOption{true, 0, ""},
 		},
 		"-p only": {
 			flags:    []string{"-p", "8080"},
 			expected: &Option{false, 8080, ""},
 		},
+		"-p only another": {
+			flags:    []string{"-p", "8080"},
+			expected: &AnotherOption{false, 8080, ""},
+		},
 		"-d only": {
 			flags:    []string{"-d", "/usr/logs"},
-			expected:  &Option{false, 0, "/usr/logs"},
+			expected: &Option{false, 0, "/usr/logs"},
+		},
+		"-d only another": {
+			flags:    []string{"-d", "/usr/logs"},
+			expected: &AnotherOption{false, 0, "/usr/logs"},
 		},
 		"multiple flags '-l -p 9090 -d /usr/vars'": {
 			flags:    []string{"-l", "-p", "9090", "-d", "/usr/vars"},
-			expected:  &Option{true, 9090, "/usr/vars"},
+			expected: &Option{true, 9090, "/usr/vars"},
+		},
+		"multiple flags '-l -p 9090 -d /usr/vars' another": {
+			flags:    []string{"-l", "-p", "9090", "-d", "/usr/vars"},
+			expected: &AnotherOption{true, 9090, "/usr/vars"},
 		},
 	}
 
@@ -64,41 +84,4 @@ type AnotherOption struct {
 	L bool
 	P int
 	D string
-}
-
-func TestParseAnotherOption(t *testing.T) {
-	testcases := map[string]struct {
-		flags    []string
-		expected interface{}
-	}{
-		"no flags should get default value": {
-			flags:    []string{},
-			expected: &AnotherOption{},
-		},
-		"-l only should set true for bool field": {
-			flags:    []string{"-l"},
-			expected: &AnotherOption{true, 0, ""},
-		},
-		"-p only": {
-			flags:    []string{"-p", "8080"},
-			expected: &AnotherOption{false, 8080, ""},
-		},
-		"-d only": {
-			flags:    []string{"-d", "/usr/logs"},
-			expected: &AnotherOption{false, 0, "/usr/logs"},
-		},
-	}
-
-	for name, tt := range testcases {
-		t.Run(name, func(t *testing.T) {
-			// 注意:并发问题
-			tt := tt
-			// 利用多核,并行运行
-			t.Parallel()
-
-			actual := NewActual(tt.expected)
-			args.Parse(actual, tt.flags...)
-			assert.Equal(t, tt.expected, actual)
-		})
-	}
 }
