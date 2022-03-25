4ea4d09c59234882a5dac3617fe1a63e72d7841f
longyue0521 <longyueli0521@gmail.com>
2022-03-25 12:32:19 +0800

    重构：将obj.CanSet()移动到循环外，将所有continue去掉

--- args/args.go
+++ args/args.go
@@ -7,25 +7,23 @@ import (
 
 func Parse(v interface{}, flags ...string) {
 	obj := reflect.ValueOf(v).Elem()
+	if !obj.CanSet() {
+		return
+	}
 	for i, flag := range flags {
 		if flag == "-l" {
-			if obj.CanSet() && obj.Type().Field(0).IsExported() {
+			if obj.Type().Field(0).IsExported() {
 				obj.Field(0).SetBool(true)
-				continue
 			}
 		} else if flag == "-p" {
-			if obj.CanSet() && obj.Type().Field(1).IsExported() {
+			if obj.Type().Field(1).IsExported() {
 				p, _ := strconv.Atoi(flags[i+1])
 				obj.Field(1).SetInt(int64(p))
-				continue
 			}
 		} else if flag == "-d" {
-			if obj.CanSet() && obj.Type().Field(2).IsExported() {
+			if obj.Type().Field(2).IsExported() {
 				obj.Field(2).SetString(flags[i+1])
-				continue
 			}
 		}
 	}
 }
-
-
