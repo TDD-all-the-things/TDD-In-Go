9b9466c939c1a16aba405d1efa626732bcd8632a
longyue0521 <longyueli0521@gmail.com>
2022-03-25 16:22:23 +0800

    重构：编写等效代码使代码中的坏味道"重复"更明显，同时将一个隐藏逻辑表达出来——通过obj.Field().Type().String()来判断字段的类型才能知道如何解析标志

--- args/args.go
+++ args/args.go
@@ -10,26 +10,40 @@ func Parse(v interface{}, flags ...string) {
 	if !obj.CanSet() {
 		return
 	}
+
+	var val interface{}
 	if obj.Type().Field(0).IsExported() {
+		val = false
 		for _, flag := range flags {
-			if flag == "-l" {
-				obj.Field(0).SetBool(true)
+			if flag == "-l" && obj.Field(0).Type().String() == "bool" {
+				val = true
 			}
 		}
+		if val != nil {
+			obj.Field(0).Set(reflect.ValueOf(val))
+		}
 	}
+
+	val = (interface{})(nil)
 	if obj.Type().Field(1).IsExported() {
 		for i, flag := range flags {
-			if flag == "-p" {
-				p, _ := strconv.Atoi(flags[i+1])
-				obj.Field(1).SetInt(int64(p))
+			if flag == "-p" && obj.Field(1).Type().String() == "int" {
+				val, _ = strconv.Atoi(flags[i+1])
 			}
 		}
+		if val != nil {
+			obj.Field(1).Set(reflect.ValueOf(val))
+		}
 	}
+	val = (interface{})(nil)
 	if obj.Type().Field(2).IsExported() {
 		for i, flag := range flags {
-			if flag == "-d" {
-				obj.Field(2).SetString(flags[i+1])
+			if flag == "-d" && obj.Field(2).Type().String() == "string" {
+				val = flags[i+1]
 			}
+		}
+		if val != nil {
+			obj.Field(2).Set(reflect.ValueOf(val))
 		}
 	}
 }
