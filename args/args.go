a17f64953f4d4187c5d58250dd679a54ea9c7a00
longyue0521 <longyueli0521@gmail.com>
2022-03-28 23:02:24 +0800

    绿红：修改Parse及parseOption的返回值列表，声明ErrMissingTag变量等使编译通过——绿灯但一闪而过，测试正常运行但断言失败故红灯亮起

--- args/args.go
+++ args/args.go
@@ -1,27 +1,37 @@
 package args
 
 import (
+	"errors"
 	"reflect"
 	"strconv"
 
 	"github.com/longyue0521/TDD-In-Go/args/parser"
 )
 
-func Parse(v interface{}, options ...string) {
+var (
+	ErrMissingTag = errors.New("missing tag")
+)
+
+func Parse(v interface{}, options ...string) error {
 	obj := reflect.ValueOf(v).Elem()
 
 	for i := 0; i < obj.NumField(); i++ {
 		field := obj.Type().Field(i)
 		if field.IsExported() {
-			obj.Field(i).Set(reflect.ValueOf(parseOption(field, options)))
+			val, err := parseOption(field, options)
+			if err != nil {
+				return err
+			}
+			obj.Field(i).Set(reflect.ValueOf(val))
 		}
 	}
+	return nil
 }
 
-func parseOption(field reflect.StructField, options []string) interface{} {
+func parseOption(field reflect.StructField, options []string) (interface{}, error) {
 	p := PARSERS[field.Type.String()]
-	value, _ := p.Parse(options, field.Tag.Get("args"))
-	return value
+	value, err := p.Parse(options, field.Tag.Get("args"))
+	return value, err
 }
 
 var PARSERS map[string]parser.OptionParser = map[string]parser.OptionParser{
