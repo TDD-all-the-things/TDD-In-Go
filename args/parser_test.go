cfe96c6601f3f28266d58212c580badcb4ea66f3
longyue0521 <longyueli0521@gmail.com>
2022-03-28 18:12:00 +0800

    红灯：添加"只传入-l标志/选项，但有参数跟随"测试，驱动产品代码处理sad path

---
+++ args/parser_test.go
@@ -0,0 +1,15 @@
+package args_test
+
+import (
+	"testing"
+
+	"github.com/longyue0521/TDD-In-Go/args"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBoolOptionParser_WithExtraArgument_ReturnsError(t *testing.T) {
+	options, option := []string{"-l", "t"}, "l"
+	value, err := args.BoolOptionParser().Parse(options, option)
+	assert.Nil(t, value)
+	assert.ErrorIs(t, err, args.ErrTooManyArguments)
+}
